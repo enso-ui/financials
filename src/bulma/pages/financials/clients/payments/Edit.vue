3efaf4265f0df5c935459173b95a5867c55821b7
Mihai <mihai.ocneanu@gmail.com>
2020-02-10 13:38:28 +0200

    added documents & comments to client payments

--- src/bulma/pages/financials/clients/payments/Edit.vue
+++ src/bulma/pages/financials/clients/payments/Edit.vue
@@ -1,5 +1,37 @@
 <template>
-    <base-form/>
+    <div>
+        <base-form/>
+        <div class="columns is-centered">
+            <div class="column is-three-quarters-desktop is-full-touch">
+                <accessories>
+                    <template slot-scope="{ count }">
+                        <tab keep-alive
+                             id="Comments">
+                            <div class="columns is-centered">
+                                <div class="column is-two-thirds">
+                                    <comments :id="id"
+                                        type="clientPayment"
+                                        @update="$set(count, 'Comments', $refs.comments.count)"
+                                        ref="comments"/>
+                                </div>
+                            </div>
+                        </tab>
+                        <tab keep-alive
+                             id="Documents">
+                            <div class="columns is-centered">
+                                <div class="column is-two-thirds">
+                                    <documents :id="id"
+                                       type="clientPayment"
+                                       @update="$set(count, 'Documents', $refs.documents.count)"
+                                       ref="documents"/>
+                                </div>
+                            </div>
+                        </tab>
+                    </template>
+                </accessories>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
